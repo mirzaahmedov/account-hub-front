525810eebdf82412cea98099d79aa46633a415b3
Mirzaahmedov Bekzod <mirzaahmedov.dev@gmail.com>
2026-06-15 09:00:37 +0500

    fix validation

--- src/pages/register/data.ts
+++ src/pages/register/data.ts
@@ -3,6 +3,6 @@ import z from "zod";
 export const UserCreateSchema = z.object({
   name: z.string().nonempty("Required field"),
   email: z.email(),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string().nonempty("Required field"),
 });
 export type UserCreateForm = z.infer<typeof UserCreateSchema>;
